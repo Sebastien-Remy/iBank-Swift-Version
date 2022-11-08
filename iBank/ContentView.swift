3684da4fa580bce8885bb3310e637963c90f58c9
Sébastien REMY <55869882+Sebastien-Remy@users.noreply.github.com>
2022-11-08 18:29:11 +0100

    add category, third, project list / detail view

--- iBank/ContentView.swift
+++ iBank/ContentView.swift
@@ -14,18 +14,53 @@ struct ContentView: View {
     var body: some View {
         NavigationSplitView {
             VStack {
+                Divider()
                 AccountListingView()
+                Divider()
                 CategoryListingView()
+                Divider()
+                ThirdListingView()
+                Divider()
+                ProjectListingView()
+                Spacer()
             }
             .frame(minWidth: 250)
             
         } detail: {
-            if let selectedAccount = dataController.selectedAccount {
-                AccountDetailView(account: selectedAccount)
-                    .padding()
-            } else {
-                Text("Please select a review")
+            switch dataController.currentSelection {
+            case .account:
+                if let selectedAccount = dataController.selectedAccount {
+                    AccountDetailView(account: selectedAccount)
+                        .padding()
+                } else {
+                    Text("Please select an account")
+                }
+            case .category:
+                if let selectedCategory = dataController.selectedCategory {
+                    CategoryDetailView(category: selectedCategory)
+                        .padding()
+                } else {
+                    Text("Please select a category")
+                }
+            case .third:
+                if let selectedThird = dataController.selectedThird {
+                    ThirdDetailView(third: selectedThird)
+                        .padding()
+                } else {
+                    Text("Please select a third")
+                }
+            case .project:
+                if let selectedProject = dataController.selectedProject {
+                    ProjectDetailView(project: selectedProject)
+                        .padding()
+                } else {
+                    Text("Please select a project")
+                }
+            default:
+                Text("Select something on the left panel")
             }
+        
+    
         }
         
     }
