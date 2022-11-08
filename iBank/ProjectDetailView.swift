c1faf939fd8e755a3eb97d61dea03c5da17317f2
Sébastien REMY <55869882+Sebastien-Remy@users.noreply.github.com>
2022-11-08 18:23:43 +0100

    initial commit

---
+++ iBank/ProjectDetailView.swift
@@ -0,0 +1,34 @@
+//
+//  ProjectDetailView.swift
+//  iBank
+//
+//  Created by Sebastien REMY on 08/11/2022.
+//
+
+import SwiftUI
+
+struct ProjectDetailView: View {
+    @ObservedObject var project: Project
+    @EnvironmentObject var dataController: DataController
+    
+    var body: some View {
+        Form {
+            TextField("Project: ", text: $project.projectName)
+        }
+        // Save on Change
+        .onChange(of: project.projectName, perform: dataController.enqueueSave)
+        
+        // Disable when project deleted
+        .disabled(project.managedObjectContext == nil)
+    }
+}
+
+struct ProjectDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        let dataController = DataController()
+        let project = Project(context: dataController.container.viewContext,
+                          name: "Tes")
+        ProjectDetailView(project: project)
+            .environmentObject(dataController)
+    }
+}
