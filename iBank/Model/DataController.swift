f26250a42be9c865b2c34f44dd9191e58eb3e773
Sébastien REMY <55869882+Sebastien-Remy@users.noreply.github.com>
2022-11-09 21:48:01 +0100

    minor

--- iBank/Model/DataController.swift
+++ iBank/Model/DataController.swift
@@ -23,7 +23,6 @@ class DataController: ObservableObject {
                     selectedThird = nil
                     selectedProject = nil
                     currentSelection = .account
-                    
                 }
         }
     }
